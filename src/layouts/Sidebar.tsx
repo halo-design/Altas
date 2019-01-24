ac43ae17e4c273cf445d9ad6cdfb819ce55e7f64
OwlAford <244734834@qq.com>
2019-01-24 18:05:09 +0800

    🌈 Daily Update

--- src/layouts/Sidebar.tsx
+++ src/layouts/Sidebar.tsx
@@ -5,11 +5,12 @@ const Sidebar = () => (
   <footer className="app-menu-sidebar">
     <div className="app-logo" />
     <div className="menu-button-group">
-      <NavLink exact={true} to="/home" activeClassName="active">home</NavLink>
-      <NavLink exact={true} to="/upload" activeClassName="active">upload</NavLink>
-      <NavLink exact={true} to="/mine" activeClassName="active">mine</NavLink>
-      <NavLink exact={true} to="/device" activeClassName="active">device</NavLink>
-      <NavLink exact={true} to="/sync" activeClassName="active">sync</NavLink>
+      <NavLink exact={true} to="/home" className="iconfont" activeClassName="active">&#xec89;</NavLink>
+      <NavLink exact={true} to="/upload" className="iconfont" activeClassName="active">&#xe725;</NavLink>
+      <NavLink exact={true} to="/mine" className="iconfont" activeClassName="active">&#xe741;</NavLink>
+      <NavLink exact={true} to="/device" className="iconfont" activeClassName="active">&#xe602;</NavLink>
+      <NavLink exact={true} to="/tools" className="iconfont" activeClassName="active">&#xe83d;</NavLink>
+      <NavLink exact={true} to="/sync" className="iconfont" activeClassName="active">&#xe626;</NavLink>
     </div>
   </footer>
 )
