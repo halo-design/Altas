11dcc43136c38fa17f3ae6b5f42214dfa665d429
OwlAford <244734834@qq.com>
2019-02-26 13:42:23 +0800

    🌈 Daily Update

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,11 +1,40 @@
 import * as React from 'react';
 
 class HomeView extends React.Component {
+  public recordStream: any = null;
+  private videoEl: any = null;
+  private camReady : boolean = false;
+
+  public componentDidMount () {
+    navigator.mediaDevices.getUserMedia({ video: {  width: 480, height: 320 } })
+      .then((stream) => {
+        this.camReady = true;
+        if (this.videoEl) {
+          this.videoEl.srcObject = stream;
+          this.recordStream = stream;
+        }
+      })
+      .catch((err => {
+        console.log('设备不支持视频录制');
+      }));
+    }
+
+    public componentWillUnmount () {
+    this.videoEl.pause();
+    this.videoEl.currentTime = 0;
+    if (this.camReady) {
+      this.recordStream.getTracks().forEach((track: any): void => {
+        track.stop();
+      });
+    }
+  }
+
   public render() {
     return (
       <div className="App">
         <div className="logo" />
         <h1>Awesome Nail!</h1>
+        <video ref={node => { this.videoEl = node }} autoPlay={true} muted={true} />
       </div>
     )
   }
