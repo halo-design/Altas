8a5682b6294a58f52ed48df4f7ca8923635188ac
OwlAford <244734834@qq.com>
2019-06-04 18:57:03 +0800

    🌈 Change Details

--- src/frontend/views/Runner/index.tsx
+++ src/frontend/views/Runner/index.tsx
@@ -1,19 +1,17 @@
 import * as React from 'react';
 import { inject, observer } from 'mobx-react';
 import { action, observable } from 'mobx';
-// import Select from 'antd/lib/select';
 import Input from 'antd/lib/input';
 import Icon from 'antd/lib/icon';
-// import message from 'antd/lib/message';
-// import Switch from 'antd/lib/switch';
+import message from 'antd/lib/message';
+import notification from 'antd/lib/notification';
 import { selectFile } from '../../utils/file';
-// const { Option } = Select;
-// const InputGroup = Input.Group;
 
 import './index.scss';
 
 @inject((stores: any) => {
   return {
+    kill: () => stores.terminal.kill(),
     shell: (str: string) => stores.terminal.shell(str),
   };
 })
@@ -26,18 +24,42 @@ class RunnerView extends React.Component<any, any> {
     selectFile(
       {
         message: '选择工程目录',
-        properties: ['openDirectory', 'createDirectory', 'promptToCreate'],
+        properties: ['openDirectory'],
       },
       (res: string[] | undefined) => {
         if (res) {
           this.projectPath = res[0];
           el.value = res[0];
+          this.props.shell(`cd ${this.projectPath}\n`);
         }
       }
     );
   }
 
+  public commander(str: string) {
+    const { shell } = this.props;
+    if (this.projectPath) {
+      notification.success({
+        message: '已运行',
+        description: '请勿重复操作.',
+      });
+      shell(str);
+    } else {
+      message.warn('请选择工程目录！');
+    }
+  }
+
+  public killProcess() {
+    this.props.kill();
+    setTimeout(() => {
+      message.info('当前进程已结束！');
+      this.props.shell(`cd ${this.projectPath}\n`);
+    }, 1000);
+  }
+
   public render() {
+    const isWin = process.platform === 'win32';
+
     return (
       <div className="sub-page-runner">
         <div className="form-table">
@@ -54,6 +76,75 @@ class RunnerView extends React.Component<any, any> {
                 }}
                 suffix={<Icon type="project" />}
               />
+            </div>
+          </div>
+          <div className="form-item">
+            <div className="label">开发命令选项</div>
+            <div className="comand-table">
+              {!isWin && (
+                <div className="row">
+                  <div className="desc">工程目录sudo授权（需输入密码）</div>
+                  <div className="control">
+                    <div
+                      className="btn-default"
+                      onClick={() => {
+                        this.commander(
+                          `sudo chmod -R 777 ${this.projectPath}\n`
+                        );
+                      }}
+                    >
+                      运行
+                    </div>
+                  </div>
+                </div>
+              )}
+              <div className="row">
+                <div className="desc">本地开发模式</div>
+                <div className="control">
+                  <div
+                    className="btn-default btn-kill"
+                    onClick={() => {
+                      this.killProcess();
+                    }}
+                  >
+                    结束
+                  </div>
+                  <div
+                    className="btn-default"
+                    onClick={() => {
+                      this.commander('npm run serve\n');
+                    }}
+                  >
+                    运行
+                  </div>
+                </div>
+              </div>
+              <div className="row">
+                <div className="desc">项目构建打包</div>
+                <div className="control">
+                  <div
+                    className="btn-default"
+                    onClick={() => {
+                      this.commander('npm run build\n');
+                    }}
+                  >
+                    运行
+                  </div>
+                </div>
+              </div>
+              <div className="row">
+                <div className="desc">单元模块测试</div>
+                <div className="control">
+                  <div
+                    className="btn-default"
+                    onClick={() => {
+                      this.commander('npm run test:unit\n');
+                    }}
+                  >
+                    运行
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
